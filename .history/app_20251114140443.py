87a7dce5a64f288cefb863d73155e58f4fdf1102
haris <abdulharisasari4@gmail.com>
2025-11-14 14:15:52 +0700

    first commit

---
+++ .history/app_20251114140443.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# GET endpoint
+@app.get("/hello")
+def hello():
+    return jsonify({
+        "message": "Hello World!",
+        "status": "success"
+    })
+
+# POST endpoint
+@app.post("/submit")
+def submit():
+    data = request.json  # menerima JSON body
+    return jsonify({
+        "received": data,
+        "status": "success"
+    })
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=2881)
